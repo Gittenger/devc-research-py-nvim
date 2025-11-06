1934f3e4298daf83dc4a042070d3e128433e8f1d
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2025-11-06 09:52:23 -0500

    formatting

--- scripts/alt-start.sh
+++ scripts/alt-start.sh
@@ -2,10 +2,10 @@
 docker build -t py-nvim-sandbox -f .devcontainer/Dockerfile .
 
 docker run -d --name py-nvim-dev \
--v "$(pwd)":/workspace \
--v "$HOME/.config/nvim":/root/.config/nvim:ro \
--v "$HOME/.local/share/nvim":/root/.local/share/nvim \
--w /workspace \
-py-nvim-sandbox bash -lc "sleep infinity"
+	-v "$(pwd)":/workspace \
+	-v "$HOME/.config/nvim":/root/.config/nvim:ro \
+	-v "$HOME/.local/share/nvim":/root/.local/share/nvim \
+	-w /workspace \
+	py-nvim-sandbox bash -lc "sleep infinity"
 
 docker exec -it py-nvim-dev bash -lc 'poetry install --no-root && nvim .'
